2b18932f92dc33c2f577b4c8ca158e4f68658aa2
inhwanK <inhan1009@naver.com>
2023-02-06 23:03:37 +0900

    feat: api 추가

--- src/main/java/com/example/purple/controller/OembedController.java
+++ src/main/java/com/example/purple/controller/OembedController.java
@@ -4,30 +4,32 @@ import ac.simons.oembed.OembedResponse;
 import ac.simons.oembed.OembedService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
 @Slf4j
-@RestController("/api")
+@RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class OembedController {
 
     private final OembedService oembedService;
 
     @GetMapping("/oembed")
-    public OembedResponse getOembedResponse(
-            @RequestParam(required = true) String url,
+    public Optional<OembedResponse> getOembedResponse(
+            @RequestParam String url,
             @RequestParam(required = false) int maxwidth,
             @RequestParam(required = false) int minwidth,
-            @RequestParam(required = true) String format
+            @RequestParam(defaultValue = "json", required = true) String format
     ) {
 
         Optional<OembedResponse> response = oembedService.getOembedResponseFor(url);
 
-        log.info("this is oembed : {}", response);
-        return response.get();
+        return response;
     }
 }
